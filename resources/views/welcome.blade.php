d2be2dcf5439b6062e2d2743935a7668b7a86514
Luis Mosqueda <isaac.mosquedag@gmail.com>
2026-06-05 16:04:21 -0600

    Prueba real de despliegue automatico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CrossoverMX | Gestión de Torneos</title>
+    <title>CrossoverMX | Gestión de Torneo</title>
     
     <!-- Fuente: Outfit -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
